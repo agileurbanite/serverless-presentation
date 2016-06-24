272340c48b77c0ddec0b6fef5fa53405ed7f2056
keu754 <patrick.woo@capitalone.com>
2016-06-24 08:55:33 -0400

    Serverless pres

---
+++ myNewApi/index.js
@@ -0,0 +1,8 @@
+var package = require("./package.json"),
+  myNewApi = require("./lib/my_new_api.js");
+
+console.log("loaded: " + package.name + ", version: " + package.version);
+
+exports.handler = function(event, context) {
+  myNewApi.handleRequest(event, context.done);
+};
